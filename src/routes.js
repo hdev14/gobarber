adb1368f18e2aacdf6dc00a4840c166c507ba00e
HermersonDev <ne.hermerson@gmail.com>
2020-02-21 09:52:59 -0300

    Config sequelize

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: "Hellow Debug" });
+  return res.json({ message: 'Hellow Debug' });
 });
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
 import server from './app';
 
-server.listen(3000, () => {
-    console.log('Server ON');
-});
+server.listen(3000);
