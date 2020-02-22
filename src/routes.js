63dace0ea48bbd906de3f1902774939047ab2054
HermersonDev <ne.hermerson@gmail.com>
2020-02-22 11:56:49 -0300

    Add user update route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+
+// CONTROLLERS
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+
+// MIDDLEWARES
+import auth from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(auth);
+
+routes.put('/users', UserController.update);
 
 export default routes;
