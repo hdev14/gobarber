11c59ff1b39e78510df6c50ca8f2f9101733e576
HermersonDev <ne.hermerson@gmail.com>
2020-02-23 20:56:23 -0300

    :construction: File controller

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import multerConfig from './config/multer';
 // CONTROLLERS
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 // MIDDLEWARES
 import auth from './app/middlewares/auth';
@@ -22,8 +23,6 @@ routes.use(auth);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
