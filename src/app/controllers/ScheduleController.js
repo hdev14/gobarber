e92f54c6114e6911afdfd79d6b307830675aecbc
HermersonDev <ne.hermerson@gmail.com>
2020-02-24 11:45:23 -0300

    :construction: list provider appointments

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,6 +1,35 @@
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
 class ScheduleController {
   async index(req, res) {
-    return res.json();
+    const isUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!isUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const passedDate = parseISO(req.query.date);
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(passedDate), endOfDay(passedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
   }
 }
 
