f059bfd1f9fae9817c4b62d2c7732e9abf6b8bea
HermersonDev <ne.hermerson@gmail.com>
2020-02-25 14:26:02 -0300

    :wrench: nodemailer configs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b958880643ad11',
+    pass: 'b909df1eed3919',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com.br>',
+  },
+};
