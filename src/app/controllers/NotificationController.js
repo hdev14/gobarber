429480952bb5b96d1ed91f18acfdb08b83f8b60a
HermersonDev <ne.hermerson@gmail.com>
2020-02-25 11:07:09 -0300

    :construction: list notificaitons

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,8 +1,24 @@
+import User from '../models/User';
 import Notification from '../schemas/Notification';
 
 class NotificationController {
   async index(req, res) {
-    return res.json();
+    const isProvider = await User.findOne({
+      id: req.userId,
+      provider: true,
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notifications' });
+    }
+
+    const notifications = await Notification.find({ user: req.userId })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
   }
 }
 
