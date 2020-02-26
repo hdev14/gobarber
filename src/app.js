803b3112b19ab37f5d7cba95a34b83f8eb3e3f31
HermersonDev <ne.hermerson@gmail.com>
2020-02-26 14:38:46 -0300

    :construction: method exceptionHandler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { resolve } from 'path';
-import * as Sentry from '@sentry/node';
+import Youch from 'youch';
 
+import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
 
+import 'express-async-errors';
 import routes from './routes';
 
 import './database/index';
@@ -14,6 +16,7 @@ class App {
     Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -28,6 +31,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.json(errors);
+    });
   }
 }
 
