3836117905810756e58b91ccea83486ebc85372a
HermersonDev <ne.hermerson@gmail.com>
2020-03-22 22:17:51 -0300

    :bug: fix the checkPassword always true

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -22,7 +22,7 @@ class SessionController {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    if (!user.checkPassword(password)) {
+    if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
